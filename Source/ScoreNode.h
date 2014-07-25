60b0b6189bbf877d9645d3aa86ca0a18a518ba1b
李翌文 <johnnyeven@gmail.com>
2014-07-26 00:28:44 +0800

    1

---
+++ Source/ScoreNode.h
@@ -0,0 +1,13 @@
+//
+//  ScoreNode.h
+//  bacterial2
+//
+//  Created by 李翌文 on 14-7-26.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface ScoreNode : CCNode
+
+@end
---
+++ Source/ScoreNode.m
@@ -0,0 +1,33 @@
+//
+//  ScoreNode.m
+//  bacterial2
+//
+//  Created by 李翌文 on 14-7-26.
+//  Copyright (c) 2014年 Apportable. All rights reserved.
+//
+
+#import "ScoreNode.h"
+#import "PZLabelScore.h"
+
+@implementation ScoreNode
+{
+    PZLabelScore *lblScore;
+    int score;
+}
+
+-(void)btnCloseTouch
+{
+    
+}
+
+-(void)btnResetTouch
+{
+    
+}
+
+-(void)btnMenuTouch
+{
+    
+}
+
+@end
