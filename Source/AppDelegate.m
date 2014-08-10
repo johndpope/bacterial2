6fd6eac1b8a7ae508e0aa6c5f78a412b1a70736b
李翌文 <johnnyeven@gmail.com>
2014-08-11 00:03:56 +0800

    1

--- Source/AppDelegate.m
+++ Source/AppDelegate.m
@@ -45,6 +45,14 @@
 #import "MainScene.h"
 #import "GuideScene.h"
 
+//for mac
+#include <sys/socket.h>
+#include <sys/sysctl.h>
+#include <net/if.h>
+#include <net/if_dl.h>
+
+//for idfa
+#import <AdSupport/AdSupport.h>
 #import <StoreKit/StoreKit.h>
 
 #define dataStorageManager [DataStorageManager sharedDataStorageManager]
@@ -103,6 +111,14 @@
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didLoadVersionConfig:) name:@"requestVersionConfig" object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didConnectFailed:) name:@"connectionError1009" object:nil];
     [[PZWebManager sharedPZWebManager] asyncGetRequest:@"http://b2.profzone.net/configuration/version_config" withData:nil];
+    
+    NSString * appKey = @"53d22a9c56240b9ab2080b1c";
+    NSString * deviceName = [[[UIDevice currentDevice] name] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString * mac = [self macString];
+    NSString * idfa = [self idfaString];
+    NSString * idfv = [self idfvString];
+    NSString * urlString = [NSString stringWithFormat:@"http://log.umtrack.com/ping/%@/?devicename=%@&mac=%@&idfa=%@&idfv=%@", appKey, deviceName, mac, idfa, idfv];
+    [NSURLConnection connectionWithRequest:[NSURLRequest requestWithURL: [NSURL URLWithString:urlString]] delegate:nil];
     
     return YES;
 }
@@ -479,24 +495,24 @@
     #endif
     
     CCScene *scene = [CCScene node];
-//    if (dataStorageManagerGuide)
-//    {
-//        int guideStep = dataStorageManagerGuideStep;
-//        guideStep = fmax(1, guideStep);
-//        
-//        GuideScene *guide;
-//        if(iPhone5)
-//        {
-//            guide = (GuideScene *)[CCBReader load:@"GuideScene-r4"];
-//        }
-//        else
-//        {
-//            guide = (GuideScene *)[CCBReader load:@"GuideScene"];
-//        }
-//        guide.guideStep = guideStep;
-//        [scene addChild:guide];
-//    }
-//    else
+    if (dataStorageManagerGuide)
+    {
+        int guideStep = dataStorageManagerGuideStep;
+        guideStep = fmax(1, guideStep);
+        
+        GuideScene *guide;
+        if(iPhone5)
+        {
+            guide = (GuideScene *)[CCBReader load:@"GuideScene-r4"];
+        }
+        else
+        {
+            guide = (GuideScene *)[CCBReader load:@"GuideScene"];
+        }
+        guide.guideStep = guideStep;
+        [scene addChild:guide];
+    }
+    else
     {
         MainScene *mainScene;
         if(iPhone5)
@@ -524,6 +540,98 @@
 -(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
 {
     [UMessage didReceiveRemoteNotification:userInfo];
+}
+
+- (NSString * )macString{
+    int mib[6];
+    size_t len;
+    char *buf;
+    unsigned char *ptr;
+    struct if_msghdr *ifm;
+    struct sockaddr_dl *sdl;
+    
+    mib[0] = CTL_NET;
+    mib[1] = AF_ROUTE;
+    mib[2] = 0;
+    mib[3] = AF_LINK;
+    mib[4] = NET_RT_IFLIST;
+    
+    if ((mib[5] = if_nametoindex("en0")) == 0) {
+        printf("Error: if_nametoindex error\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 1\n");
+        return NULL;
+    }
+    
+    if ((buf = malloc(len)) == NULL) {
+        printf("Could not allocate memory. error!\n");
+        return NULL;
+    }
+    
+    if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
+        printf("Error: sysctl, take 2");
+        free(buf);
+        return NULL;
+    }
+    
+    ifm = (struct if_msghdr *)buf;
+    sdl = (struct sockaddr_dl *)(ifm + 1);
+    ptr = (unsigned char *)LLADDR(sdl);
+    NSString *macString = [NSString stringWithFormat:@"%02X:%02X:%02X:%02X:%02X:%02X",
+                           *ptr, *(ptr+1), *(ptr+2), *(ptr+3), *(ptr+4), *(ptr+5)];
+    free(buf);
+    
+    return macString;
+}
+
+- (NSString *)idfaString {
+    
+    NSBundle *adSupportBundle = [NSBundle bundleWithPath:@"/System/Library/Frameworks/AdSupport.framework"];
+    [adSupportBundle load];
+    
+    if (adSupportBundle == nil) {
+        return @"";
+    }
+    else{
+        
+        Class asIdentifierMClass = NSClassFromString(@"ASIdentifierManager");
+        
+        if(asIdentifierMClass == nil){
+            return @"";
+        }
+        else{
+            
+            //for no arc
+            //ASIdentifierManager *asIM = [[[asIdentifierMClass alloc] init] autorelease];
+            //for arc
+            ASIdentifierManager *asIM = [[asIdentifierMClass alloc] init];
+            
+            if (asIM == nil) {
+                return @"";
+            }
+            else{
+                
+                if(asIM.advertisingTrackingEnabled){
+                    return [asIM.advertisingIdentifier UUIDString];
+                }
+                else{
+                    return [asIM.advertisingIdentifier UUIDString];
+                }
+            }
+        }
+    }
+}
+
+- (NSString *)idfvString
+{
+    if([[UIDevice currentDevice] respondsToSelector:@selector( identifierForVendor)]) {
+        return [[UIDevice currentDevice].identifierForVendor UUIDString];
+    }
+    
+    return @"";
 }
 
 @end
